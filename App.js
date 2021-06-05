f44618cf73557935f1294371ea357a75b5434b48
yogeshwari-jhala <yogeshwarijhala1627@gmail.com>
2021-06-05 19:18:00 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import Header from "./src/Components/Header.component";
+import Footer from "./src/Components/Footer.component";
+import Note from "./src/Components/Note.component";
+import "./styles.css";
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/Components/Note.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Note = () => {
+  return (
+    <div className="note">
+      <h1>Javascript and ReactJS</h1>
+      <p>
+        This was an amazing bootcamp taken by Shaurya Sinha we covered
+        everything from scratch including ReactJS, Javascript, HTMl
+      </p>
+    </div>
+  );
+};
+export default Note;
